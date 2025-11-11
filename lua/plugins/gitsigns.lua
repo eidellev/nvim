39721f95ec8f2f78523d457a0f3333f4db087375
lev eidelman <lev.eidelman@gong.io>
2025-11-11 14:47:58 +0200

    added inline git blame

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,13 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true, -- Enable inline blame
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- end of line
+      delay = 500, -- delay in ms before blame appears
+      ignore_whitespace = true,
+    },
+    current_line_blame_formatter = "<author>, <author_time:%R> • <summary>",
+  },
+}
