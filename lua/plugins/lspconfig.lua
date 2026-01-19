d3d2c64ea5fdbddc371fcdba6ec139033f652b40
lev eidelman <lev.eidelman@gong.io>
2026-01-19 11:15:50 +0200

    give vtsls extra memory

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,14 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = function(_, opts)
+    opts.servers.vtsls = vim.tbl_deep_extend("force", opts.servers.vtsls or {}, {
+      settings = {
+        typescript = {
+          tsserver = {
+            maxTsServerMemory = 8192,
+          },
+        },
+      },
+    })
+  end,
+}
