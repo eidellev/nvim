41ff4fc81188aa2eef93b8c6ab7c649cd75e7124
Lev Eidelman Nagar <lev.eidelman@gong.io>
2026-07-07 11:11:00 +0300

    changed some icons

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,10 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
+    opts.codelens = {
+      enabled = true,
+    }
+
     opts.servers.vtsls = vim.tbl_deep_extend("force", opts.servers.vtsls or {}, {
       cmd = { "vtsls", "--stdio", "--max-old-space-size=8192" },
       settings = {
@@ -8,7 +12,18 @@ return {
           tsserver = {
             maxTsServerMemory = 8192,
           },
+          referencesCodeLens = {
+            enabled = true,
+            showOnAllFunctions = true, -- Optional: shows on internal functions too, not just exported ones
+          },
+          implementationsCodeLens = {
+            enabled = true,
+          },
         },
+      },
+      javascript = {
+        referencesCodeLens = { enabled = true },
+        implementationsCodeLens = { enabled = true },
       },
       vtsls = {
         -- Prevent vtsls from eagerly checking status on all files
--- lua/plugins/mini.icons.lua
+++ lua/plugins/mini.icons.lua
@@ -5,7 +5,11 @@ return {
       extension = {
         css = { glyph = "", hl = "MiniIconsPurple" }, -- Replace with your desired icon
         less = { glyph = "", hl = "MiniIconsBlue" }, -- Replace with your desired icon
-        mdx = { glyph = "", hl = "MiniIconsBlue" }, -- Replace with your desired icon
+        mdx = { glyph = "", hl = "MiniIconsRed" }, -- Replace with your desired icon
+        md = { glyph = "", hl = "MiniIconsBlue" }, -- Replace with your desired icon
+      },
+      file = {
+        ["README.md"] = { glyph = "󰈙", hl = "MiniIconsGrey" },
       },
     },
   },
